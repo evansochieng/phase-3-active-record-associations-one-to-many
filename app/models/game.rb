b32c19dbec0b3b4a6915febaadb6c60df07627d5
Evans Ochieng <evansochieng121@gmail.com>
2022-11-03 14:19:20 +0300

    learn one-to-many association in ar

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,4 @@
 class Game < ActiveRecord::Base
-  
+  #add the has_many macro
+  has_many :reviews
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,4 @@
 class Review < ActiveRecord::Base
-  
+    #use the belongs to macros
+  belongs_to :game
 end
